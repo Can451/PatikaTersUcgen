0e30391c80895fabbb0127cf8a066efc66511f82
Can Yeşiltepe <cyesiltepe9@gmail.com>
2022-04-12 16:15:20 +0300

    PatikaTersUcgen

---
+++ PatikaTersÜçgenProje/src/TersÜcgen.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TersÜcgen {
+    public static void main(String[] args) {
+        // Java ile basamak sayısının kullanıcıdan alınan ve döngüler kullanılarak,
+        //yıldızlar(*) ile ekrana ters üçgen çizen programı yazın.
+
+        //satır=n 1 2 3 4 5
+        //boşluk=i-1; 1 2 3 4 5 6 7
+        /*
+           yıldız= 9-7-5-3-1
+         */
+
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("Deger giriniz: ");
+        int n = inp.nextInt();
+
+
+        for (int i=1;i<=n;i++){
+            for (int k=0;k<=i;k++) {
+                System.out.print(" ");
+            }
+            for (int j=n*2-1;j>=i*2-1;j--){
+                System.out.print("*");
+            }
+
+            System.out.println(" ");
+
+        }
+    }
+}
